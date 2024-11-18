48ec0f505e7ba7c164ffd3acd2b5a3791b13566c
manish <mmandlekar88@gmail.com>
2024-11-18 14:15:01 +0530

    updated

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -7,15 +7,14 @@ const Category = () => {
   const tableData = [
     {
       id: 1,
-      title: '#4356',
       Image: 'Hat',
       role: '10x Developer',
-      content:'Matt Dickersons',
-      category: 'javascript',
-      slug: 'Transfer Bank',
+      description:'Matt Dickersons',
+      parentCategory: 'javascript',
       action: 'Approved',
       avatar: 'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
     },
+    
     // ... add more data entries as needed
   ];
 
@@ -30,11 +29,11 @@ const Category = () => {
         </h2>
       
 
-  <Link to='/createBlog'>
+  {/* <Link to='/createBlog'>
   <button className='bg-[#7e3af2] text-white px-4 py-2 rounded-md'>
-        + Create Blog
+        + Create Category
       </button>
-  </Link>
+  </Link> */}
         </div>
        
 
@@ -48,18 +47,15 @@ const Category = () => {
             <table className="w-full whitespace-no-wrap">
               <thead>
                 <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                    <th className="px-4 py-3">Title</th>
-                  <th className="px-4 py-3">Image</th>
-                  <th className="px-4 py-3">Content</th>
-                  <th className="px-4 py-3">Category</th>
-                  <th className="px-4 py-3">Slug</th>
+                  <th className="px-4 py-3">Name</th>
+                  <th className="px-4 py-3">description</th>
+                  <th className="px-4 py-3">parent Category</th>
                   <th className="px-4 py-3">Action</th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                 {tableData.map((user) => (
                   <tr key={user.id} className="text-gray-700 dark:text-gray-400">
-                    <td className="px-4 py-3">{user.title}</td>
                     <td className="px-4 py-3">
                       <div className="flex items-center text-sm">
                         <div className="relative hidden w-8 h-8 mr-3 rounded-full md:block">
@@ -82,9 +78,8 @@ const Category = () => {
                         </div>
                       </div>
                     </td>
-                    <td className="px-4 py-3 text-sm">{user.content}</td>
-                    <td className="px-4 py-3 text-sm">{user.category}</td>
-                    <td className="px-4 py-3 text-sm">{user.slug}</td>
+                    <td className="px-4 py-3 text-sm">{user.description}</td>
+                    <td className="px-4 py-3 text-sm">{user.parentCategory}</td>
                     <td className="px-4 py-3 text-xs flex gap-2">
                        <Link to='/editBlog'>
                        <button>
